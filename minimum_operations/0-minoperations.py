0164dbf85965681b1c21af37cecf9ab49a3b1255
Albert499 <a.logozaga@alustudent.com>
2024-09-14 13:10:51 -0700

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+
+"""
+Given a number n, write a method that calculates the fewest # of operations.
+
+Prototype: def minOperations(n)
+Returns an integer
+If n is impossible to achieve, return 0
+"""
 
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+def minOperations(n):
+    if n <= 1:
         return 0
-    return op
+    factors = []
+    a = 2
+    while a * a <= n:
+        while (n % a) == 0:
+            factors.append(a)
+            n //= a
+        a += 1
+    if n > 1:
+        factors.append(n)
+    # Calculate the minimun operations using the prime factors
+    min_operations = sum(factors)
+    return min_operations
